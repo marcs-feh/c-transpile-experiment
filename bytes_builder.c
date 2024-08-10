0f80b6c47386fd868239c934595b6d8477771db0
marcs-feh <marcosfehlauer@protonmail.com>
2024-08-10 17:55:54 -0300

    byte builder

--- bytes_builder.c
+++ bytes_builder.c
@@ -18,15 +18,36 @@ void builder_destroy(Bytes_Builder* bb){
 	bb->data = 0;
 }
 
-bool builder_push_bytes(Bytes_Builder* bb, byte* const b, isize len){
+bool builder_push_bytes(Bytes_Builder* bb, byte* const bytes, isize len){
+	if(bb->len + len > bb->cap){
+		bool status = builder_resize(bb, bb->cap * 2);
+		if(!status){ return false; }
+	}
+	mem_copy(&bb->data[bb->len], bytes, len);
+	return true;
 }
 
-bool builder_resize(Bytes_Builder* bb, isize new_size){}
+bool builder_resize(Bytes_Builder* bb, isize new_size){
+	byte* new_data = New(byte, new_size, bb->allocator);
+	if(new_data == NULL){ return false; }
+
+	mem_copy(new_data, bb->data, Min(new_size, bb->len));
+	mem_free(bb->allocator, bb->data);
+	bb->data = new_data;
+	bb->cap = new_size;
+
+	return true;
+}
 
 void builder_reset(Bytes_Builder* bb){
-	mem_set(bb->data, 0, bb->len);
 	bb->len = 0;
+	mem_set(bb->data, 0, bb->cap);
 }
 
-byte* builder_build(Bytes_Builder* bb, Mem_Allocator allocator){}
+byte* builder_build(Bytes_Builder* bb, Mem_Allocator allocator){
+	byte* buf = New(byte, bb->len, allocator);
+	if(buf == NULL){ return NULL; }
+	builder_reset(bb);
+	return buf;
+}
 
--- main.c
+++ main.c
@@ -9,6 +9,5 @@ int main(){
 	Mem_Arena arena;
 	arena_init(&arena, MAIN_MEM, sizeof(MAIN_MEM));
 	Mem_Allocator allocator = arena_allocator(&arena);
-
 }
 
